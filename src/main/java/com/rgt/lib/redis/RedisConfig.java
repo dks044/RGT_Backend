014822858f6ea61a5cc2218e6ef3cdb0dbe35d58
AJN <buzz781@gmail.com>
2024-12-07 00:57:12 +0900

    feat: 데이터베이스 설정(레디스, mysql)

---
+++ src/main/java/com/rgt/lib/redis/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.rgt.lib.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+    @Value("${spring.data.redis.url}") 
+    private String redisUrl;
+
+    @Value("${spring.data.redis.password}") 
+    private String redisPassword;
+
+    @Bean
+    RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisUrl);
+        config.setPassword(redisPassword);
+        return new LettuceConnectionFactory(config);
+    }
+    
+    @Bean
+    StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+}
